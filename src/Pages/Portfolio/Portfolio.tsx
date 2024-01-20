a82fef304e41399b7e00352f0482171de86cd3ba
Siddharth Karanchery <webdev.siddharth@gmail.com>
2024-01-20 23:34:31 +0530

    moved UI portfolio data to backend

--- src/Pages/Portfolio/Portfolio.tsx
+++ src/Pages/Portfolio/Portfolio.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import { Box } from "@mui/material";
 
 import "./Portfolio.css";
-import { uiData } from "../../Data/ui";
 
 import PortfolioSwiper from "../../Components/Portfolio Page Components/PortfolioSwiper/PortfolioSwiper";
 import { webdevData } from "../../Data/webdev";
@@ -20,6 +19,7 @@ function Portfolio() {
   const [digitalArtData, setDigitalArtData] = React.useState<
     { [x: string]: any }[]
   >([]);
+  const [uIData, setUIData] = React.useState<{ [x: string]: any }[]>([]);
 
   const fetchSketches = async () => {
     await getDocs(collection(db, "Sketches")).then((result) => {
@@ -39,13 +39,23 @@ function Portfolio() {
     });
   };
 
+  const fetchUI = async () => {
+    await getDocs(collection(db, "UI")).then((result) => {
+      const data = result.docs.map((doc) => ({
+        ...doc.data(),
+      }));
+      setUIData(data);
+    });
+  };
+
   React.useEffect(() => {
     fetchSketches();
     fetchDigitalArt();
+    fetchUI();
   }, []);
   return (
     <Box className="Portfolio">
-      <PortfolioSwiper title={"UI Design"} data={uiData} />
+      <PortfolioSwiper title={"UI Design"} data={uIData} />
       <PortfolioSwiper title={"Web Development"} data={webdevData} />
       <PortfolioSwiper title={"Paintings"} data={paintingData} />
       <PortfolioSwiper title={"Sketches"} data={sketchesData} />
