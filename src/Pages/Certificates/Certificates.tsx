d69655de650d3bc32f1636696331ef934e8d6d5a
Siddharth Karanchery <webdev.siddharth@gmail.com>
2024-01-21 19:59:12 +0530

    moved certificates data to backend

--- src/Pages/Certificates/Certificates.tsx
+++ src/Pages/Certificates/Certificates.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 import { Grid } from "@mui/material";
 
 import js from "../../assets/certificates/js.jpg";
@@ -9,10 +11,28 @@ import sql from "../../assets/certificates/sql.jpg";
 import noImage from "../../assets/certificates/noImage.jpg";
 
 import "./Certificates.css";
-import { certificatesData } from "../../Data/certificates";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 import CertificatesCard from "../../Components/Certificates Page Components/CertificatesCard/CertificatesCard";
 
 function Certificates() {
+  const [certificatesData, seCertificatesData] = React.useState<
+    { [x: string]: any }[]
+  >([]);
+
+  const fetchCertificates = async () => {
+    await getDocs(collection(db, "Certificates")).then((result) => {
+      const data = result.docs.map((doc) => ({
+        ...doc.data(),
+      }));
+      seCertificatesData(data);
+    });
+  };
+
+  React.useEffect(() => {
+    fetchCertificates();
+  }, []);
+
   const fileSelector = (filename: string) => {
     switch (filename) {
       case "js":
